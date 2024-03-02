cb89c30f429ec2a64253e04837df8ac214fd9c45
leleo-arch <leo_xdplay@hotmail.com>
2024-03-02 15:17:08 -0300

    fs

---
+++ Index.js
@@ -0,0 +1,72 @@
+const myprojects = []
+const express = require ('express')
+const app = express()
+app.use(express.json())
+const uuid = require('uuid')
+const port = 5000
+
+const chekprojectid = (request, respons, next ) => {
+    const {id} = request.params
+    const index = myprojects.findIndex(item => item.id === id)
+    if (index < 0){
+        return respons.status(404).json({messagem: "erros not existente"})
+        }
+        request.projectindex = index
+        next()
+}
+
+app.get('/myprojects/', (request, response) => {
+    return response.json(myprojects)
+}) 
+
+
+app.post('/myprojects', (request, response) => {
+    const {order, clientNam, price, status} = request.body
+    const project = { id: uuid.v4(), order, clientNam, price, status}
+    myprojects.push(project)
+    return response.status(201).json(project)
+    
+})
+
+app.put('/myprojects/:id', (request, response) => {
+    const {id} = request.params
+    const {order, clientName, price, status} = request.body
+    const index = request.projectindex
+    const updateproject = {id, order, clientName, price, status}
+    myprojects[index] = updateproject
+    return response.json(updateproject)
+    
+});
+
+app.delete('/myprojects/:id', chekprojectid, (request, response) => {
+       const index = request.projectindex
+        myprojects.splice(index,1)
+        return response.status(204).json()
+}) 
+
+ app.patch('/myprojects/:id', (request, response) => {
+    const {id} = request.params
+    const {status, order, clientName, price} = request.body
+    const updateproject = {id, order, clientName, price, status}
+    const index = myprojects.findIndex(item => item.id === id)
+    if (index < 0){
+    return response.status(404).json({messagem: "erros not existente"})
+    }
+    myprojects[index] = updateproject
+    return response.json(updateproject)
+    
+});
+
+app.get(':id', (request, response) => {
+    const {id} = request.params
+    const updateproject = {id}
+    const index = myprojects.findIndex(item => item.id === id)
+    if (index < 0){
+    return response.status(404).json({messagem: "erros not existente"})
+    }
+    myprojects[index] = updateproject 
+    return response.json(updateproject)
+    
+});
+
+app.listen(5000)
